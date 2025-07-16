df3e78d13d424932579f746aa6c2fc7fbb0c1135
Weronika Wojtak <w.wojtak@gmail.com>
2025-07-16 11:40:50 +0100

    new launch files

---
+++ src/dnf_cognitive_architecture_extended/launch/dnf_learn_basic_launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='dnf_cognitive_architecture_extended',
+            executable='input_matrix',
+            name='input_matrix'
+        ),
+        Node(
+            package='dnf_cognitive_architecture_extended',
+            executable='dnf_model_learning_basic',
+            name='dnf_model_learning_basic'
+        )
+    ])
